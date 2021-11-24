509fc6eb65a85b2a01e9fbbd5b9c6612f887975a
VladNCode <vldsmrfow@gmail.com>
2021-11-24 08:08:41 +0200

    added allowed updates util func

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,13 +2,46 @@ const Task = require('../models/taskModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+const allowedUpdates = require('../utils/allowedUpdates');
 // const APIFeatures = require('../utils/apiFeatures');
 
 exports.getAllTasks = factory.getAll(Task);
 exports.getTask = factory.getOne(Task);
 exports.createTask = factory.createOne(Task);
-exports.updateTask = factory.updateOne(Task);
+// exports.updateTask = factory.updateOne(Task);
 // exports.deleteTask = factory.deleteOne(Task);
+
+exports.updateTask = catchAsync(async (req, res, next) => {
+  allowedUpdates(req, res, next, 'description', 'completed', 'age');
+
+  // const updates = Object.keys(req.body);
+  // const allowedUpdates = ['description', 'completed'];
+  // const isValidOperation = updates.every(item => allowedUpdates.includes(item));
+  // if (!isValidOperation)
+  //   return next(
+  //     new AppError(
+  //       `You can only update DESCSRIPTION and COMPLETED by using this route! Please exclude everything else if you want to proceed`,
+  //       404
+  //     )
+  //   );
+
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task)
+    return next(
+      new AppError(`No task found with this id ${req.params.id}`, 404)
+    );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      task,
+    },
+  });
+});
 
 exports.deleteTask = catchAsync(async (req, res, next) => {
   const task = await Task.findByIdAndDelete(req.params.id);
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+const allowedUpdates = require('../utils/allowedUpdates');
 
 exports.createUser = catchAsync(async (req, res, next) => {
   const user = await User.create({
@@ -23,16 +24,7 @@ exports.getUser = factory.getOne(User);
 exports.getAllUsers = factory.getAll(User);
 
 exports.updateUser = catchAsync(async (req, res, next) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'age'];
-  const isValidOperation = updates.every(item => allowedUpdates.includes(item));
-  if (!isValidOperation)
-    return next(
-      new AppError(
-        `You can only update NAME and AGE by using this route! Please exclude everything else if you want to proceed`,
-        404
-      )
-    );
+  allowedUpdates(req, res, next, 'name', 'age');
 
   const user = await User.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
