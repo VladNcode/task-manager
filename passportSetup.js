0f9c0024a1e97f3dd28c616fbb05effb2ec9b6c7
VladNCode <vldsmrfow@gmail.com>
2021-11-29 10:04:00 +0200

    changed passport setup for heroku

--- passportSetup.js
+++ passportSetup.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://127.0.0.1:3000/auth/google/callback',
+      callbackURL: 'https://vlad-taskapp.herokuapp.com/auth/google/callback',
     },
     async function (accessToken, refreshToken, profile, done) {
       // console.log(accessToken);
