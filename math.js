5f80ff0c6a4ef132a4c8bcf04b23039596bdd6fe
VladNCode <vldsmrfow@gmail.com>
2021-11-29 11:29:28 +0200

    started to learn how to test with jest

---
+++ math.js
@@ -0,0 +1,7 @@
+const calcTip = (total, tipPercent = 0.25) => total + total * tipPercent;
+
+const fahrenheitToCelsius = temp => (temp - 32) / 1.8;
+
+const celsiusToFahrenheit = temp => temp * 1.8 + 32;
+
+module.exports = { calcTip, fahrenheitToCelsius, celsiusToFahrenheit };
