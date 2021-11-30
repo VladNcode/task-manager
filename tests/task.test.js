3b6123e78435040c7db60d1313279899a4cdbbce
VladNCode <vldsmrfow@gmail.com>
2021-11-30 09:36:12 +0200

    added jest tests for users and tasks

---
+++ tests/task.test.js
@@ -0,0 +1,90 @@
+require('dotenv').config({ path: './test.env' });
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+const Task = require('../models/taskModel');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+const userOne = { name: 'Pikachu', age: '25', email: 'pika@example.com', password: 'test1234' };
+
+let token;
+let taskId;
+
+//* Connect to the database
+beforeAll(async () => {
+  await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connect(DB);
+
+  await mongoose.connection.db.dropDatabase();
+  await User.create(userOne);
+});
+
+beforeEach(async () => {
+  const res = await request(app)
+    .post('/api/v1/users/login')
+    .send({ email: 'pika@example.com', password: 'test1234' });
+  token = res.body.token;
+
+  const res2 = await request(app)
+    .post('/api/v1/tasks/')
+    .send({ description: 'BURGIR', completed: true })
+    .set('Authorization', 'Bearer ' + token)
+    .expect(201);
+
+  taskId = res2.body.data.task._id;
+});
+
+afterEach(async () => {
+  await Task.deleteOne({ _id: taskId });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+test('Should be able to get all user tasks', async () => {
+  await request(app)
+    .get('/api/v1/tasks/')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to create new task', async () => {
+  await request(app)
+    .post('/api/v1/tasks/')
+    .send({ description: 'BURGIR BURGIR', completed: true })
+    .set('Authorization', 'Bearer ' + token)
+    .expect(201);
+});
+
+test('Should be able to get task', async () => {
+  await request(app)
+    .get('/api/v1/tasks/' + taskId)
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to update task', async () => {
+  await request(app)
+    .patch('/api/v1/tasks/' + taskId)
+    .send({ completed: false })
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to delete task', async () => {
+  await request(app)
+    .delete('/api/v1/tasks/' + taskId)
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to visit main page', async () => {
+  await request(app).get('/').expect(200);
+});
--- tests/user.test.js
+++ tests/user.test.js
@@ -2,20 +2,112 @@ require('dotenv').config({ path: './test.env' });
 const request = require('supertest');
 const app = require('../app');
 const mongoose = require('mongoose');
+const User = require('../models/userModel');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+const userOne = { name: 'Pikachu', age: '25', email: 'pika@example.com', password: 'test1234' };
+const userTwo = { name: 'Bulbasaur', age: '25', email: 'bulba@example.com', password: 'test1234' };
+
+let token;
+let id;
+
 //* Connect to the database
-mongoose
-  .connect(DB, {
+beforeAll(async () => {
+  await mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(console.log('Connected to DB'));
+  });
 
-test('Should signup a new user', async () => {
+  mongoose.connect(DB);
+
+  await mongoose.connection.db.dropDatabase();
+});
+
+beforeEach(async () => {
+  await User.create(userOne);
+
+  const res = await request(app)
+    .post('/api/v1/users/login')
+    .send({ email: 'pika@example.com', password: 'test1234' });
+  token = res.body.token;
+  // console.log(res.body);
+  id = res.body.data.user._id;
+});
+
+afterEach(async () => {
+  await User.deleteOne({ _id: id });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+test('Should be able to signup a new user', async () => {
+  await request(app).post('/api/v1/users/').send(userTwo).expect(201);
+});
+
+test('Should be able to login', async () => {
   await request(app)
-    .post('/api/v1/users/')
-    .send({ name: 'Pikachu', age: '25', email: 'pika@example.com', password: 'test1234' })
-    .expect(201);
+    .post('/api/v1/users/login')
+    .send({ email: 'pika@example.com', password: 'test1234' })
+    .expect(200);
+});
+
+test('Should be able to get /me page', async () => {
+  await request(app)
+    .get('/api/v1/users/me')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to get all users', async () => {
+  await request(app)
+    .get('/api/v1/users/')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to get one user', async () => {
+  await request(app)
+    .get('/api/v1/users/' + id)
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to update user', async () => {
+  await request(app)
+    .patch('/api/v1/users/' + id)
+    .send({ name: 'Bulb' })
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to logout', async () => {
+  await request(app)
+    .get('/api/v1/users/logout')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to update password', async () => {
+  await request(app)
+    .patch('/api/v1/users/updatePassword')
+    .send({ email: 'pika@example.com', password: 'test1234', newPassword: 'test1234' })
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to delete me', async () => {
+  await request(app)
+    .delete('/api/v1/users/deleteMe')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(204);
+});
+
+test('Should be able to delete user', async () => {
+  await request(app)
+    .delete('/api/v1/users/' + id)
+    .set('Authorization', 'Bearer ' + token)
+    .expect(204);
 });
