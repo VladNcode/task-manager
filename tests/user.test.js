48400eaecde40f8138186c964fd9e9b1b3c5ae8e
VladNCode <vldsmrfow@gmail.com>
2021-12-01 08:22:12 +0200

    added more user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,12 +3,14 @@ const request = require('supertest');
 const app = require('../app');
 const mongoose = require('mongoose');
 const User = require('../models/userModel');
+const { expect } = require('@jest/globals');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 const userOne = { name: 'Pikachu', age: '25', email: 'pika@example.com', password: 'test1234' };
 const userTwo = { name: 'Bulbasaur', age: '25', email: 'bulba@example.com', password: 'test1234' };
 
+const url = '127.0.0.1:3000';
 let token;
 let id;
 
@@ -42,23 +44,26 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-// test('Should not be able to login with wrong credentials', async () => {
-//   await request('https://vlad-taskapp.herokuapp.com')
-//     .post('/api/v1/users/login/')
-//     .send({ email: 'test@example.com', password: 'test12345' })
-//     .expect(401);
-// }, 2000);
+test('Should not be able to login with wrong credentials', async () => {
+  await request(url)
+    .post('/api/v1/users/login/')
+    .send({ email: 'test@example.com', password: 'test12345' })
+    .expect(401);
+});
 
-// ? Not working in development
-// test('Should not be able to login with wrong credentials', async () => {
-//   request(app)
-//     .post('/api/v1/users/login/')
-//     .send({ email: 'test@example.com', password: 'test12345' })
-//     .expect(200);
-// });
+test.only('Should be able to signup a new user', async () => {
+  const res = await request(app).post('/api/v1/users/').send(userTwo).expect(201);
+  const user = await User.findOne({ email: 'bulba@example.com' });
 
-test('Should be able to signup a new user', async () => {
-  await request(app).post('/api/v1/users/').send(userTwo).expect(201);
+  // Checking if user is in DB
+  expect(user).not.toBeNull();
+
+  // Checking if req contains correct info
+  expect(res.body.data.user).toMatchObject({
+    name: 'Bulbasaur',
+    age: 25,
+    email: 'bulba@example.com',
+  });
 });
 
 test('Should be able to login', async () => {
@@ -73,6 +78,10 @@ test('Should be able to get /me page', async () => {
     .get('/api/v1/users/me')
     .set('Authorization', 'Bearer ' + token)
     .expect(200);
+});
+
+test('Should not be able to get /me page while unauthenticated', async () => {
+  await request(url).get('/api/v1/users/me').expect(401);
 });
 
 test('Should be able to get all users', async () => {
@@ -93,6 +102,14 @@ test('Should be able to update user', async () => {
   await request(app)
     .patch('/api/v1/users/' + id)
     .send({ name: 'Bulb' })
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200);
+});
+
+test('Should be able to upload avatar', async () => {
+  await request(app)
+    .patch('/api/v1/users/' + id)
+    .attach('avatar', 'public/img/avatars/user-61a1d72c77ea5407e1111a3b-1638008819594.jpeg')
     .set('Authorization', 'Bearer ' + token)
     .expect(200);
 });
@@ -119,9 +136,19 @@ test('Should be able to delete me', async () => {
     .expect(204);
 });
 
+test('Should not be able to delete me while unauthenticated', async () => {
+  await request('127.0.0.1:3000').delete('/api/v1/users/deleteMe').expect(401);
+});
+
 test('Should be able to delete user', async () => {
   await request(app)
     .delete('/api/v1/users/' + id)
     .set('Authorization', 'Bearer ' + token)
     .expect(204);
+});
+
+test('Should not be able to delete user while unauthenticated', async () => {
+  await request('127.0.0.1:3000')
+    .delete('/api/v1/users/' + id)
+    .expect(401);
 });
